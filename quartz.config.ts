e3004c79e5efe51310389f842ef254982983a532
urbanquixote <urbanquixote@gmail.com>
2025-08-30 23:25:27 +0900

    타이틀변경v3

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Urban Quixote",
+    pageTitle: "디지털가든",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
